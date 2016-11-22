d5cf85bea53b40de4ebd496069838ac7c2b2d4bf
Jesse van Meel <jesse@MacBook-Pro-van-Jesse.local>
2016-11-22 12:20:14 +0100

    globals and triggers
    global variables, adding order counter and asked counter, added
    assignment function trigger

--- rogerbot.py
+++ rogerbot.py
@@ -5,22 +5,70 @@
 import chatServer as c
 import random
 
+def sleep(n):
+    c.sleep(n)
+
+def output(s):
+    c.output(s)
+
 def setup():
-    c.output("Hello, friend")
-    c.sleep(1)
-    c.output('What can I do for you?')
+    global askedCounter
+    global orderCounter
+    askedCounter = 0
+    orderCounter = 0
+    output("Hello, friend")
+    sleep(1)
+    output('What can I do for you?')
 
 def response(input):
     #print(input)
-    respondRandom()
+    if not respondToTrigger(input):
+        output(randomResponse())
+    # if respondToTrigger(input):
+    #     pass
+    # else:
+    #     output(randomResponse())
 
-def respondRandom():
+def randomResponse():
+    global orderCounter
     answers = [
     "Ok!",
     "You got it!",
     "Concider it done",
     "I'm already working on it!",
     "Yes, naturally.",
-    "Ofcourse mate!"
+    "Ofcourse mate!",
+    "Processing"
+
     ]
-    c.output(random.choice(answers))
+    response = answers[orderCounter]
+    orderCounter += 1
+    if orderCounter >= len(answers):
+        orderCounter=0
+    return response
+
+def randomAssignment():
+    answers = [
+    "The assignment is to make a ChatBot, like me :)",
+    "Code a working ChatBot",
+    "Your ChatBot should talk to people",
+    "You should code a Bot to chat with in Python"
+    ]
+    return random.choice(answers)
+
+def respondToTrigger(input):
+    global askedCounter
+    trigger = [
+    "assignment",
+    "to do",
+    "explain",
+    "should i do"
+    ]
+    for t in trigger:
+        if t in input:
+            if askedCounter >1:
+                output("You should understand by now...")
+            output(randomAssignment())
+            askedCounter += 1
+            return True
+    return False
